da51885a4b7060b3d10bca678230fd6ba12f1f27
Ronaldo Goncalves <rgoncalves1026@gmail.com>
2021-04-29 20:52:43 +0100

    edit and update added to the project

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,23 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+  
+  end
+
   private
 
   def item_params
